2295befd4f4da60aaa8c3e6cfd8f575ca60fb1d6
Quinn Campbell <qcampbell@yodle.com>
2017-06-14 17:26:30 -0700

    more slider work

--- site/js/site.js
+++ site/js/site.js
@@ -1,9 +1,55 @@
+// Slide Panels
 $(document).ready(function() {
-	$('a.workout').click(function() {
-	  $('.slide-nav.workout').animate( {
-	    left: "+=250",
-	  }, 300, function() {
+
+
+	$('a:contains("Daily To Do")').click(function() {
 	    
-	  });
+	    $('.workout ul').animate({
+	        opacity: 0
+		}, {duration: 300, complete: function(){
+			$('.workout h2').animate({
+				height: 0,
+				opacity: 1
+			}, {duration: 200, complete: function(){
+				$('.workout').animate({
+					width: 0,
+					opacity: 0
+				});
+			}});
+		}});
+
+	 //    $('.workout').animate({
+	 //        width: 250,
+	 //        marginRight: 0,
+	 //        opacity: 1
+		// }, {duration: 300, complete: function(){
+		// 	$('.workout h2').animate({
+		// 		height: 28,
+		// 		opacity: 1
+		// 	}, {duration: 200, complete: function(){
+		// 		$('.workout ul').animate({
+		// 			opacity: 1
+		// 		}, {duration: 200});
+		// 	}});
+		// }});
 	});
+
+	$('a:contains("Workout")').click(function() {
+	    $('.workout').animate({
+	        width: 250,
+	        opacity: 1
+		}, {duration: 300, complete: function(){
+			$('.workout h2').animate({
+				height: 28,
+				opacity: 1
+			}, {duration: 200, complete: function(){
+				$('.workout ul').animate({
+					opacity: 1
+				}, {duration: 200});
+			}});
+		}});
+	});
+
+
+
 });
