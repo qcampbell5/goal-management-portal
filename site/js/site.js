f62670d05ce943c3989140e55c695754e363943c
Quinn Campbell <qcampbell@yodle.com>
2017-06-20 17:02:42 -0700

    more edits

--- site/js/site.js
+++ site/js/site.js
@@ -1,22 +1,15 @@
 // Slide Panels
-
-
-
-$('.slide-nav').css({'height':($('.main-nav').height()+'px')});
-
-if($('.workout').css('width') == '250px') {
-	$('.content-wrapper').css({'marginLeft': '250px'});
-};
-
-
 $(document).ready(function() {
-
+	$('.slide-nav').css({'height':($('.main-nav').height()+'px')});
 	if ($('a:contains("Daily To Do")').hasClass('active')){
 		$('a:contains("Daily To Do")').ready(function() { 
 		    $('.daily-to-do').animate({
 		        width: 250,
 		        opacity: 1
 			}, {duration: 500, complete: function(){
+				if($('.daily-to-do').css('width') == '250px') {
+					$('.content-wrapper').css({'marginLeft': '250px'});			
+				};				
 				$('.daily-to-do h2').animate({
 					height: 28,
 					opacity: 1
@@ -34,6 +27,14 @@ $(document).ready(function() {
 		        width: 250,
 		        opacity: 1
 			}, {duration: 500, complete: function(){
+				if($('.workout').css('width') == '250px') {
+					$('.content-wrapper').css({'marginLeft': '250px'});
+		if ($('.workout').css('width') == '250px') {
+			alert('yes');
+			// $('.movement-tile').fadeTo('slow', 1);
+		}					
+					// workoutPanel();						
+				};				
 				$('.workout h2').animate({
 					height: 28,
 					opacity: 1
@@ -41,9 +42,12 @@ $(document).ready(function() {
 					$('.workout ul').animate({
 						opacity: 1
 					}, {duration: 100});
+	
 				}});
 			}});
 		}); 
+
 	}// end Workout panel slide
-	else {}
+	else {};
+
 });
---
+++ site/js/workout-panel.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	function  workoutPanel() {
+		if ($('.workout').css('width') == '250px') {
+			alert('yes');
+			// $('.movement-tile').fadeTo('slow', 1);
+		}
+	}
+
+});
