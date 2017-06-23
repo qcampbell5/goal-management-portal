1a76e4f0c8d254988a625339463c41f4358d26da
Quinn Campbell <qcampbell@yodle.com>
2017-06-23 16:27:57 -0700

    more edits

--- site/js/site.js
+++ site/js/site.js
@@ -8,32 +8,40 @@ $(document).ready(function() {
 
 	// start horizontal slide nav function
 	function slideNavHoriz() {
-		if ($('a:contains("Daily To Do")').hasClass('active')){		
-			if ($('.daily-to-do').hasClass('slide-nav-vert')) {
-				$(this).removeClass('slide-nav-vert');
-				$(this).addClass('slide-nav-horiz', 500, function(){
+		if ($('a:contains("Daily To Do")').hasClass('active')){
+			if ($('.daily-to-do').hasClass('sn-v')) {
+				$('.daily-to-do').css('width', '100%');
+				$('.content-wrapper').addClass('cw-h', 500);
+				$(this).removeClass('sn-v');
+				$(this).addClass('sn-h', 500, function(){
 					$('h2').addClass('show-text', 200);
 					$('ul').addClass('show-text', 500);
-				});				
+				});		
 			} 
 			else {
-				$('.daily-to-do').addClass('slide-nav-horiz', 500, function(){
+				$('.daily-to-do').css('width', '100%');				
+				$('.content-wrapper').addClass('cw-h', 500);
+				$('.daily-to-do').addClass('sn-h', 500, function(){
 					$('h2').addClass('show-text', 200);
 					$('ul').addClass('show-text', 500);
-				});				
+				});		
 			}	
 		}
 		else if ($('a:contains("Workout")').hasClass('active')){		
-			if ($('.workout').hasClass('slide-nav-vert')) {
-				$(this).removeClass('slide-nav-vert');
-				$(this).addClass('slide-nav-horiz', 500, function(){
+			if ($('.workout').hasClass('sn-v')) {
+				$('.workout').css('width', '100%');
+				$('.content-wrapper').addClass('cw-h', 500);
+				$(this).removeClass('sn-v');
+				$(this).addClass('sn-h', 500, function(){
 					$('h2').addClass('show-text', 200);
 					$('ul').addClass('show-text', 500);
 				});	
 				workoutPanel();			
 			} 
 			else {
-				$('.workout').addClass('slide-nav-horiz', 500, function(){
+				$('.workout').css('width', '100%');
+				$('.content-wrapper').addClass('cw-h', 500);
+				$('.workout').addClass('sn-h', 500, function(){
 					$('h2').addClass('show-text', 200);
 					$('ul').addClass('show-text', 500);
 					workoutPanel();
@@ -41,43 +49,54 @@ $(document).ready(function() {
 			}	
 		}	
 	}// end horizontal slide nav function
+
 	function slideNavVert() {
 		if ($('a:contains("Daily To Do")').hasClass('active')){		
-			if ($('.daily-to-do').hasClass('slide-nav-horiz')) {
-				$(this).removeClass('slide-nav-horiz');
-				$(this).addClass('slide-nav-vert', 500, function(){
+			if ($('.daily-to-do').hasClass('sn-h')) {
+				$('.daily-to-do').css('height', '100%');
+				$('.content-wrapper').addClass('cw-v', 500);
+				$(this).removeClass('sn-h');
+				$(this).addClass('sn-v', 500, function(){
 					$('h2').addClass('show-text', 200);
 					$('ul').addClass('show-text', 500);
 				});				
 			} 
 			else {
-				$('.daily-to-do').addClass('slide-nav-vert', 500, function(){
+				$('.daily-to-do').css('height', '100%');
+				$('.content-wrapper').addClass('cw-v', 500);
+				$('.daily-to-do').addClass('sn-v', 500, function(){
 					$('h2').addClass('show-text', 200);
 					$('ul').addClass('show-text', 500);
 				});				
 			}	
 		}
 		else if ($('a:contains("Workout")').hasClass('active')){		
-			if ($('.workout').hasClass('slide-nav-horiz')) {
-				$(this).removeClass('slide-nav-horiz');
-				$(this).addClass('slide-nav-vert', 500, function(){
+			if ($('.workout').hasClass('sn-h')) {
+				$('.workout').css('height', '100%');
+				$('.content-wrapper').addClass('cw-v', 500);
+				$(this).removeClass('sn-h');
+				$(this).addClass('sn-v', 500, function(){
 					$('h2').addClass('show-text', 200);
 					$('ul').addClass('show-text', 500);
 				});	
 				workoutPanel();			
 			} 
 			else {
-				$('.workout').addClass('slide-nav-vert', 500, function(){
+				$('.workout').css('height', '100%');
+				$('.content-wrapper').addClass('cw-v', 500);
+				$('.workout').addClass('sn-v', 500, function(){
 					$('h2').addClass('show-text', 200);
 					$('ul').addClass('show-text', 500);
-					workoutPanel();
-				});				
+					
+				});
+				workoutPanel();			
 			}	
 		}	
 	}
 
 	if ($(window).width() < 992) {
-		slideNavHoriz();		
+		slideNavHoriz();
+				
 	}
 	else if ($(window).width() > 992) {
 		slideNavVert();
@@ -85,15 +104,15 @@ $(document).ready(function() {
 
 	$(window).resize(function(){
 		if ($(window).width() < 992) {
-			if ($('.daily-to-do').hasClass('slide-nav-vert') || $('.workout').hasClass('slide-nav-vert')) {
-				$('.daily-to-do, .workout').removeClass('slide-nav-vert');
-				$('.daily-to-do, .workout').addClass('slide-nav-horiz');			
+			if ($('.daily-to-do').hasClass('sn-v') || $('.workout').hasClass('sn-v')) {
+				$('.daily-to-do, .workout').removeClass('sn-v');
+				$('.daily-to-do, .workout').addClass('sn-h');			
 			} 		
 		}
 		else if ($(window).width() > 992) {
-			if ($('.daily-to-do').hasClass('slide-nav-horiz') || $('.workout').hasClass('slide-nav-horiz')) {
-				$('.daily-to-do, .workout').removeClass('slide-nav-horiz');
-				$('.daily-to-do, .workout').addClass('slide-nav-vert');			
+			if ($('.daily-to-do').hasClass('sn-h') || $('.workout').hasClass('sn-h')) {
+				$('.daily-to-do, .workout').removeClass('sn-h');
+				$('.daily-to-do, .workout').addClass('sn-v');			
 			} 	
 		} 
 	})
